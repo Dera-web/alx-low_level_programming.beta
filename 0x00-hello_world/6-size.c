71d1e4a95ec43589bfa7a84b7841860f43997b73
Dera-web <chideranduka27@gmail.com>
2023-02-16 20:25:23 -0800

    Task 8. Unix quote

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,21 +1,23 @@
 #include <stdio.h>
 /**
  * main - A program that prints the size of various computer types
+ * Description - C program size
  * *Return 0 if Success
 */
+
 int main(void)
 {
-char a;
-int b;
-long int c;
-long long int d;
-float f;
+	char a;
+	int b;
+	long int c;
+	long long int d;
+	float f;
 
 printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(a));
 printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(b));
 printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(c));
 printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(d));
-printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(f));
+printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(g));
 return (0);
 }
 
